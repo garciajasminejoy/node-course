488b590eac94d23aa218822d64c92a6c6588e11d
Jasmine Garcia <garciajasminejoy@gmail.com>
2021-04-01 18:27:53 +0800

    dynamic content with pug

--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     const products = adminData.products;
-    res.render('shop', { prods: products, docTitle: 'Jasmine Shop' });
+    res.render('shop', { prods: products, pageTitle: 'Jasmine Shop', path: '/' });
 });
 
 module.exports = router;
