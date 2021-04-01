49aa915927aade1ecaa601c3b743c555b2f5c759
Jasmine Garcia <garciajasminejoy@gmail.com>
2021-04-01 17:36:24 +0800

    initialize application

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<h1>Add product</h1>');
-});
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express</h1>');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
